a910ed0b53ef62ceab5eafb2218497b1a3e3b0d4
012NaveenG <012guptagkp@gmail.com>
2026-01-16 18:32:11 +0530

    add: socketio inialization

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import { app } from "./app.js";
 import dotenv from "dotenv";
-dotenv.config();
-const PORT = process.env.PORT || 5000;
+import http from "http";
+import { Server } from "socket.io";
+import { app } from "./app.js";
 
-app.listen(PORT, () =>
-  console.log(`Server is running at http://localhost:${PORT}`)
-);
+dotenv.config();
+
+const PORT = process.env.PORT || 12345;
+
+const server = http.createServer(app);
+
+
+const io = new Server(server);
+
+// Socket events
+io.on("connection", (socket) => {
+  console.log("Socket connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Socket disconnected:", socket.id);
+  });
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
